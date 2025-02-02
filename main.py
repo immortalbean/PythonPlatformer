27d5c1a4c1ae7a4cd4eb4b1a06f16ab7d8a15e0a
ImmortalBean <67293861+immortalbean@users.noreply.github.com>
2025-02-02 01:43:46 -0500

    added todo (:

--- main.py
+++ main.py
@@ -24,4 +24,5 @@ while running:
     player.tick(0)
     camera_position = pygame.Vector2(mathoperations.lerp(camera_position.x, player.position.x, 0.2), mathoperations.lerp(camera_position.y, player.position.y - 100, 0.05))
     #Move the camera movement to the player.tick()
+    #Make camera class
     render.render_level(screen, 0, camera_position, resolution, player)
